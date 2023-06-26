614ffa8148b4750d76cac35324c40a2c640025f0
Stanislav Goriainov <goriainov@ispras.ru>
2023-06-27 02:09:06 +0300

    Add doc for suffix automaton & fix errors.

--- src/string/suffix_automaton.rs
+++ src/string/suffix_automaton.rs
@@ -26,10 +26,7 @@ impl SuffixAutomaton {
     pub fn new() -> Self {
         let mut states = vec![State::default()];
         states[ROOT].link = NO_LINK;
-        Self {
-            last: ROOT,
-            states,
-        }
+        Self { last: ROOT, states }
     }
 
     /// O(string.length) time
@@ -47,6 +44,21 @@ impl SuffixAutomaton {
         self.states[ROOT].link = NO_LINK;
     }
 
+    /// O(string.len()) time
+    pub fn contains(&self, string: &str) -> bool {
+        let mut cur = ROOT;
+
+        for sym in string.chars() {
+            if self.states[cur].next.contains_key(&sym) {
+                return false;
+            }
+
+            cur = *self.states[cur].next.get(&sym).unwrap();
+        }
+
+        true
+    }
+
     /// O(string.length) time
     ///
     /// O(string.length) memory
@@ -54,35 +66,56 @@ impl SuffixAutomaton {
         for sym in string.chars() {
             self.add_char(sym);
         }
+
+        self.mark_terminal();
+    }
+
+    fn mark_terminal(&mut self) {
+        let mut prev = self.last;
+
+        while prev != NO_LINK {
+            self.states[prev].is_terminal = true;
+            prev = self.states[prev].link;
+        }
     }
 
     /// O(1) time (amortized)
-    pub fn add_char(&mut self, sym: char) {
+    fn add_char(&mut self, sym: char) {
+        // Sc - new state
         let cur_state = self.new_state();
+        // |Sc| = |S| + 1
         self.states[cur_state].length = self.states[self.last].length + 1;
 
         let mut prev = self.last;
 
         while prev != NO_LINK && !self.states[prev].next.contains_key(&sym) {
-            self.states[prev].next[&sym] = cur_state;
+            // S -->(next, c) Sc
+            // U_1 = link(S) -->(next, c) Sc ...
+            // untill U_k -->(next, c) S_0
+            *self.states[prev].next.get_mut(&sym).unwrap() = cur_state;
             prev = self.states[prev].link;
         }
 
         if prev == NO_LINK {
+            // if there's no U_k that has edge towards S_0
             self.states[cur_state].link = ROOT;
         } else {
+            // S_0 ~ next
             let next = self.states[prev].next[&sym];
 
             if self.states[prev].length + 1 == self.states[next].length {
+                // if S_0 has already been longest(next) (class representative)
                 self.states[cur_state].link = next;
             } else {
+                // If there has alredy been a transition by symbol
+                // but the existing state is not the longest in its class
                 let cloned = self.new_state();
                 self.states[cloned].length = self.states[prev].length + 1;
-                self.states[cloned].next = self.states[next].next;
+                self.states[cloned].next = self.states[next].next.clone();
                 self.states[cloned].link = self.states[next].link;
 
                 while prev != NO_LINK && self.states[prev].next[&sym] == next {
-                    self.states[prev].next[&sym] = cloned;
+                    *self.states[prev].next.get_mut(&sym).unwrap() = cloned;
                     prev = self.states[prev].link;
                 }
 
