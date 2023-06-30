3fb2290a16bbeae71ea8ee5688cc192c19fd0f06
Stanislav Goriainov <goriainov@ispras.ru>
2023-06-30 20:30:20 +0300

    Add tests for suffix automaton.

--- src/string/suffix_automaton.rs
+++ src/string/suffix_automaton.rs
@@ -20,19 +20,22 @@ pub struct SuffixAutomaton {
 impl SuffixAutomaton {
     fn new_state(&mut self) -> usize {
         self.states.push(State::default());
+
         self.states.len() - 1
     }
 
     pub fn new() -> Self {
         let mut states = vec![State::default()];
         states[ROOT].link = NO_LINK;
+
         Self { last: ROOT, states }
     }
 
-    /// O(string.length) time
+    /// O(n) time
     pub fn build(string: &str) -> Self {
         let mut ret = SuffixAutomaton::new();
         ret.add_string(string);
+
         ret
     }
 
@@ -44,12 +47,12 @@ impl SuffixAutomaton {
         self.states[ROOT].link = NO_LINK;
     }
 
-    /// O(string.len()) time
+    /// O(n) time
     pub fn contains(&self, string: &str) -> bool {
         let mut cur = ROOT;
 
         for sym in string.chars() {
-            if self.states[cur].next.contains_key(&sym) {
+            if !self.states[cur].next.contains_key(&sym) {
                 return false;
             }
 
@@ -59,9 +62,9 @@ impl SuffixAutomaton {
         true
     }
 
-    /// O(string.length) time
+    /// O(n) time
     ///
-    /// O(string.length) memory
+    /// O(n) memory
     pub fn add_string(&mut self, string: &str) {
         for sym in string.chars() {
             self.add_char(sym);
@@ -81,34 +84,24 @@ impl SuffixAutomaton {
 
     /// O(1) time (amortized)
     fn add_char(&mut self, sym: char) {
-        // Sc - new state
         let cur_state = self.new_state();
-        // |Sc| = |S| + 1
         self.states[cur_state].length = self.states[self.last].length + 1;
 
         let mut prev = self.last;
 
         while prev != NO_LINK && !self.states[prev].next.contains_key(&sym) {
-            // S -->(next, c) Sc
-            // U_1 = link(S) -->(next, c) Sc ...
-            // untill U_k -->(next, c) S_0
-            *self.states[prev].next.get_mut(&sym).unwrap() = cur_state;
+            self.states[prev].next.insert(sym, cur_state);
             prev = self.states[prev].link;
         }
 
         if prev == NO_LINK {
-            // if there's no U_k that has edge towards S_0
             self.states[cur_state].link = ROOT;
         } else {
-            // S_0 ~ next
-            let next = self.states[prev].next[&sym];
+            let next = *self.states[prev].next.get(&sym).unwrap();
 
             if self.states[prev].length + 1 == self.states[next].length {
-                // if S_0 has already been longest(next) (class representative)
                 self.states[cur_state].link = next;
             } else {
-                // If there has alredy been a transition by symbol
-                // but the existing state is not the longest in its class
                 let cloned = self.new_state();
                 self.states[cloned].length = self.states[prev].length + 1;
                 self.states[cloned].next = self.states[next].next.clone();
@@ -125,5 +118,25 @@ impl SuffixAutomaton {
         }
 
         self.last = cur_state;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn suffix_automaton_unit_1() {
+        let mut sa = SuffixAutomaton::new();
+        let strings = ["the", "a", "there", "answer", "any", "by", "bye", "their"];
+
+        for string in strings {
+            sa.add_string(string);
+        }
+
+        assert_eq!(sa.contains("the"), true);
+        assert_eq!(sa.contains("these"), false);
+        assert_eq!(sa.contains("their"), true);
+        assert_eq!(sa.contains("thaw"), false);
     }
 }
